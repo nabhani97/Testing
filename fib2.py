6b1141ac94743d600cc768a4b8043a27b2e748c3
nmercedes <43918783+nmercedes@users.noreply.github.com>
2019-06-12 10:43:23 +0100

    Add files via upload

---
+++ fib2.py
@@ -0,0 +1,24 @@
+#from typing import Dict
+#memo: Dict[int,int] = {0:0, 1:1}
+git clone https://github.com/nmercedes/Testing.git
+
+memo = {0:0, 1:1}
+# recursion or a recursive function
+def fib3(n:int) -> int:
+    if n not in memo:
+        memo[n] = fib3(n-1) + fib3(n-2)
+    return memo[n]
+
+
+
+def fib1(n:int) -> int:
+    if n < 2:
+        return n
+    return fib1(n-2) + fib1(n-1)
+
+#print (memo)
+x = fib3(50)
+#print (memo)
+
+
+print ("Result: " + str(x))
